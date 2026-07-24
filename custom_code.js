43cade671b89f249c89118762d79f63ef0377ab7
HummingbirdScientific <hummingbird@hummingbirdscientific.com>
2026-07-24 15:20:37 +0530

    Update custom_code.js

--- custom_code.js
+++ custom_code.js
@@ -32,20 +32,20 @@ function addMobileNavBackdropToBody() {
 
   if (document.querySelector("#mob-nav-backdrop-fixed")) return;
 
-  // const backdropCopy = backdrop.cloneNode(true);
-  // backdropCopy.id = "mob-nav-backdrop-fixed";
-  // // backdropCopy.style.zIndex = "889";
+  const backdropCopy = backdrop.cloneNode(true);
+  backdropCopy.id = "mob-nav-backdrop-fixed";
+  // backdropCopy.style.zIndex = "889";
 
-  // document.body.appendChild(backdropCopy);
+  document.body.appendChild(backdropCopy);
 
-  // backdropCopy.addEventListener("click", () => {
-  //   backdropCopy.style.display = "none";
-  //   backdrop.style.display = "none";
+  backdropCopy.addEventListener("click", () => {
+    backdropCopy.style.display = "none";
+    backdrop.style.display = "none";
   document.body.appendChild(backdrop);
 
-  backdrop.addEventListener("click", () => {
+  backdropCopy.addEventListener("click", () => {
+  backdropCopy.style.display = "none";
   backdrop.style.display = "none";
-  
   
 
     const mobileMenuWrapper = document.querySelector("#mobile-nav-menu");
@@ -53,14 +53,14 @@ function addMobileNavBackdropToBody() {
       mobileMenuWrapper.style.display = "none";
     }
   });
-  const mobileBurger = document.querySelector("#mobile-menu-burger-btn");
-  mobileBurger.addEventListener("click",() =>{
-    if (backdrop.style.display === "none") {
-      backdrop.style.display = "block";
-    } else {
-      backdrop.style.display = "none";
-    }
-  });
+  // const mobileBurger = document.querySelector("#mobile-menu-burger-btn");
+  // mobileBurger.addEventListener("click",() =>{
+  //   if (backdrop.style.display === "none") {
+  //     backdrop.style.display = "block";
+  //   } else {
+  //     backdrop.style.display = "none";
+  //   }
+  // });
 }
 
 function initMegaMenuNavigation() {
